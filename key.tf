98477b449129870e042290029c634794ecc8008a
Mohamed Zied Ters <ziedters@gmail.com>
2019-11-28 14:47:30 +0100

    ssh

--- key.tf
+++ key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "admin" {
    key_name   = "admin"
-   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQyU1fFlaGHJwOpTC2myjBptkgDf7FBeioFHvJEBV+L90kS08lt91hauiNsJUZNGZEFQ4392kPeXrbmF+xzWyuZA7Hi2eqwIquOjVFV7Zl1GNKv4i1DvGUBW3HJsmFhxEssRnq66XyGZ2G718ABNp40DjZwVuNhKcd8AlipbBVqWKgD3SDboDsUYZU18pp0edud8djbdYSsRKj81Hq2O49CErM6MdIeWAbZNmtWw39Xe+fOhmJ+Ej37ry/2nWyOPPmDmXn2fwo6QDFj3TGMp5zfRhNiieyYg9+XFg8XgwbbvTmsFnbV8XCGdwEtTRwnAdZOKXBHhKx6XfrtBTnmNR7"
+   public_key = ""
  }
